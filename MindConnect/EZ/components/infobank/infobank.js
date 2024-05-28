b06e54c96311f67240dd96e1f8eae07a7c031430
zacklow28 <zo.ngyun@hotmail.com>
2024-05-28 20:26:05 +0800

    Added Pic back

--- MindConnect/EZ/components/infobank/infobank.js
+++ MindConnect/EZ/components/infobank/infobank.js
@@ -25,7 +25,13 @@ const InfoBank = () => (
     <Divider  style={{backgroundColor: COLORS.oliveGreen}}/>
     <MentalIllness />
     <Divider  style={{backgroundColor: COLORS.oliveGreen}}/>
-
+    <View style = {{alignItems: "center", marginTop: 40, marginBottom: 40}}>
+      <Image
+      source={{ uri: "https://images.squarespace-cdn.com/content/v1/5de82874ac2eb4212e1c3540/e84062e0-706a-4016-a775-ed805abb3835/Mental_Illness_V_Mental_Health_V1.png" }}
+      style={{ height: 450, width: 345, borderRadius:20 }}
+      />
+    </View>
+    <Divider  style={{backgroundColor: COLORS.oliveGreen}}/>
   </View>
 );
 
